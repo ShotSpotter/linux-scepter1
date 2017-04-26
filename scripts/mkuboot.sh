a30662d9e84fd8c6bc9b9a58eaec51d6f6dd1270
cdunson <cdunson@shotspotter.com>
2017-04-26 16:08:36 -0700

    Modified to append dtb

--- scripts/mkuboot.sh
+++ scripts/mkuboot.sh
@@ -14,6 +14,17 @@ if [ -z "${MKIMAGE}" ]; then
 		exit 1;
 	fi
 fi
+echo "MKIMAGE=$MKIMAGE $@"
 
+#/home/cdunson/sst/Scepter/sysroots/x86_64-pokysdk-linux/usr/bin/mkimage -D " -O dtb -o arch/arm/boot/dts/am3517-scepter.dtb -b 0 -i arch/arm/boot/dts/ -d arch/arm/boot/dts/.am3517-scepter.dtb.d.dtc.tmp arch/arm/boot/dts/.am3517-scepter.dtb.dts.tmp" -f /home/cdunson/sst/Scepter/scepter_fdt.its arch/arm/boot/uImage.itb
+#/home/cdunson/sst/Scepter/sysroots/x86_64-pokysdk-linux/usr/bin/mkimage -f /home/cdunson/sst/Scepter/scepter_fdt.its arch/arm/boot/uImage.itb
+
+#mkimage -A arm -O linux -T kernel -C none -a 0x80008000 -e 0x80008000 -n "sst11" -d arch/arm/boot/zImage arch/arm/boot/uImage
+cat arch/arm/boot/dts/am3517-scepter.dtb >> arch/arm/boot/zImage
+#cat arch/arm/boot/dts/am3517-evm.dtb >> arch/arm/boot/zImage
+#pwd
+
+echo "KBUILD_DTBS="$KBUILD_DTBS
+echo "MKIMAGE=$MKIMAGE"
 # Call "mkimage" to create U-Boot image
 ${MKIMAGE} "$@"
